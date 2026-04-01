a758fe16deb2dbd5a355b970b5ba214f05f6963c
prahaas123 <kprahaask@gmail.com>
2026-04-01 18:12:56 -0400

    sh file update

--- slurm.sh
+++ slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=01:00:00          # Time limit: dd-hh:mm
 #SBATCH --nodes=1                # Number of nodes (1 max)
 #SBATCH --ntasks-per-node=1      # Number of tasks per node (1 max)
-#SBATCH --cpus-per-task=1       # Specify cores per node (64 max)
+#SBATCH --cpus-per-task=20       # Specify cores per node (64 max)
 #SBATCH --mem-per-cpu=4G         # Specify memory per node (0 max)
 #SBATCH --output=openvsp-scripts.log     # Output log file
 
